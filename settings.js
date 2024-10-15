e18cef2c39bba7ea09e9e1cfe15c358e4afa4182
Himasopn <129391898+Himasopn@users.noreply.github.com>
2024-10-15 22:23:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=yVFjEKYA#1Y-ECKsEflgvyuT9g4Gz9p8VffqqIAWRTlzmzqhs7h8',
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=ORkS1J6Z#C6pu2yYFQEzHMx1Yocr57evdb1ekStorq5adN8gGbkA',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '917002015750',
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
